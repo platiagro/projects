4be6c473915d92cb7d9a0eb7925d8bd334de19c5
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-09-18 12:02:39 -0300

    Add getAll Projects controller unit test

--- src/tests/components/project/controller.unit.test.js
+++ src/tests/components/project/controller.unit.test.js
@@ -7,6 +7,40 @@ import {
 } from '../../../components/project';
 
 describe('Test Project Controller methods', () => {
+  describe('Test getAll Projects controller', () => {
+    const stubProjectGetAll = sinon.stub(Model, 'getAll');
+
+    const projectGetAllVerify = async (expectedCode) => {
+      const req = httpMocks.createRequest({
+        method: 'GET',
+        url: '/projects',
+      });
+      const res = httpMocks.createResponse();
+
+      const result = await Controller.getAll(req, res);
+
+      expect(result.statusCode).toBe(expectedCode);
+    };
+
+    it('Resolves getAll model', () => {
+      stubProjectGetAll.resolves([
+        {
+          uuid: '70382be9-be20-4042-a351-31512376957b',
+          name: 'ML Example',
+          createdAt: '2019-09-17 13:41:18',
+        },
+      ]);
+
+      projectGetAllVerify(200);
+    });
+
+    it('Rejects getAll model', () => {
+      stubProjectGetAll.rejects(Error('Forced error'));
+
+      projectGetAllVerify(500);
+    });
+  });
+
   describe('Test Create Project controller', () => {
     const stubProjectCreate = sinon.stub(Model, 'create');
 
@@ -36,7 +70,7 @@ describe('Test Project Controller methods', () => {
     });
 
     it('Rejects create model', () => {
-      stubProjectCreate.rejects(Error('Force error'));
+      stubProjectCreate.rejects(Error('Forced error'));
 
       projectCreateVerify(500);
     });
