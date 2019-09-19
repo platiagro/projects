9804db37e03c35eae6ac53119d8d4874a5ed8117
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-09-19 09:11:23 -0300

    Add project getById model unit test

--- src/tests/components/project/model.unit.test.js
+++ src/tests/components/project/model.unit.test.js
@@ -4,9 +4,60 @@ import { Knex } from '../../../config';
 import { ProjectModel as Project } from '../../../components/project';
 
 describe('Test Project Model methods', () => {
-  describe('Test getAll Projects methdos', () => {
-    const stubKnex = sinon.stub(Knex, 'select');
+  const stubKnex = sinon.stub(Knex, 'select');
 
+  describe('Test getById Project method', () => {
+    const projectGetByIdVerify = (expectedError) => {
+      Project.getById('70382be9-be20-4042-a351-31512376957b')
+        .then((result) => {
+          expect(result).not.toBeNull();
+          expect(result).toEqual({
+            uuid: '70382be9-be20-4042-a351-31512376957b',
+            name: 'ML Example',
+            createdAt: '2019-09-17 13:41:18',
+          });
+        })
+        .catch((err) => {
+          expect(err).toStrictEqual(Error(expectedError));
+        });
+    };
+
+    it('', () => {
+      stubKnex.returns({
+        from: sinon.stub().returnsThis(),
+        where: sinon.stub().returnsThis(),
+        first: sinon.stub().resolves({
+          uuid: '70382be9-be20-4042-a351-31512376957b',
+          name: 'ML Example',
+          createdAt: '2019-09-17 13:41:18',
+        }),
+      });
+
+      projectGetByIdVerify(null);
+    });
+
+    it('', () => {
+      stubKnex.returns({
+        from: sinon.stub().returnsThis(),
+        where: sinon.stub().returnsThis(),
+        first: sinon.stub().resolves(undefined),
+      });
+
+      projectGetByIdVerify('Invalid UUID.');
+    });
+
+    it('', () => {
+      stubKnex.returns({
+        from: sinon.stub().returnsThis(),
+        where: sinon.stub().returnsThis(),
+        first: sinon.stub().rejects(Error('Forced error.')),
+      });
+
+      projectGetByIdVerify('Forced error.');
+    });
+  });
+
+  describe('Test getAll Projects method', () => {
     const projectGetAllVerify = () => {
       Project.getAll()
         .then((result) => {
@@ -52,7 +103,7 @@ describe('Test Project Model methods', () => {
   });
 
   describe('Test Create Project method', () => {
-    const stubKnex = sinon.stub(Knex, 'insert');
+    const stubKnexInsert = sinon.stub(Knex, 'insert');
 
     const projectCreateVerify = () => {
       Project.create(
@@ -74,7 +125,7 @@ describe('Test Project Model methods', () => {
     };
 
     it('Resolves db query', () => {
-      stubKnex.callsFake(() => {
+      stubKnexInsert.callsFake(() => {
         return {
           into: sinon.stub().resolves([0]),
         };
@@ -84,7 +135,7 @@ describe('Test Project Model methods', () => {
     });
 
     it('Rejects db query', () => {
-      stubKnex.callsFake(() => {
+      stubKnexInsert.callsFake(() => {
         return {
           into: sinon.stub().rejects(Error('Forced error')),
         };
