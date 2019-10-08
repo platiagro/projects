78ba6c860182957dc57f97a19f083219d84fac74
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-10-08 16:46:36 -0300

    Fix migrations

--- migrations/20190918132225_initial_setup.js
+++ migrations/20190918132225_initial_setup.js
@@ -11,7 +11,7 @@ exports.up = function(knex) {
         });
       }
     }),
-    knex.schema.hasTable('projects').then((exists) => {
+    knex.schema.hasTable('experiments').then((exists) => {
       if (!exists) {
         return knex.schema.createTable('experiments', function(t) {
           t.string('uuid', 255).primary();
