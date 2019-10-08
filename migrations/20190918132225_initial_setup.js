c631159918eefac9abdff8a5f5fa98ac7bd8ab59
Miguel F Ferraz <miguel.f.ferraz.27@gmail.com>
2019-10-08 15:35:26 -0300

    Update migration to verify if the table exists

--- migrations/20190918132225_initial_setup.js
+++ migrations/20190918132225_initial_setup.js
@@ -1,40 +1,75 @@
+/* eslint-disable func-names */
+/* eslint-disable consistent-return */
 exports.up = function(knex) {
   return Promise.all([
-    knex.schema.createTable('projects', function(t) {
-      t.string('uuid', 255).primary();
-      t.string('name', 255).notNull();
-      t.dateTime('createdAt').notNull();
+    knex.schema.hasTable('projects').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('projects', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('name', 255).notNull();
+          t.dateTime('createdAt').notNull();
+        });
+      }
+    }),
+    knex.schema.hasTable('projects').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('experiments', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('name', 255).notNull();
+          t.string('projectId', 255)
+            .references('uuid')
+            .inTable('projects')
+            .notNull();
+          t.string('pipelineId', 255).nullable();
+          t.string('datasetId', 255).nullable();
+          t.string('targetColumnId', 255).nullable();
+          t.text('parameters', 'longtext').nullable();
+          t.dateTime('createdAt').notNull();
+        });
+      }
     }),
 
-    knex.schema.createTable('experiments', function(t) {
-      t.string('uuid', 255).primary();
-      t.string('name', 255).notNull();
-      t.string('projectId', 255)
-        .references('uuid')
-        .inTable('projects')
-        .notNull();
-      t.string('pipelineId', 255).nullable();
-      t.string('datasetId', 255).nullable();
-      t.string('targetColumnId', 255).nullable();
-      t.text('parameters', 'longtext').nullable();
-      t.dateTime('createdAt').notNull();
+    knex.schema.hasTable('pipelines').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('pipelines', function(t) {
+          t.string('uuid', 255).primary();
+        });
+      }
     }),
 
-    knex.schema.createTable('pipelines', function(t) {
-      t.string('uuid', 255).primary();
+    knex.schema.hasTable('datasets').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('datasets', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('bucketName', 255).notNull();
+          t.string('originalName', 255).notNull();
+        });
+      }
     }),
 
-    knex.schema.createTable('datasets', function(t) {
-      t.string('uuid', 255).primary();
+    knex.schema.hasTable('headers').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('headers', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('bucketName', 255).notNull();
+          t.string('originalName', 255).notNull();
+        });
+      }
     }),
 
-    knex.schema.createTable('columns', function(t) {
-      t.string('uuid', 255).primary();
-      t.string('datasetId', 255)
-        .references('uuid')
-        .inTable('datasets')
-        .notNull();
-      t.string('dataType').notNull();
+    knex.schema.hasTable('columns').then((exists) => {
+      if (!exists) {
+        return knex.schema.createTable('columns', function(t) {
+          t.string('uuid', 255).primary();
+          t.string('headerId', 255)
+            .references('uuid')
+            .inTable('headers')
+            .notNull();
+          t.string('name', 255).notNull();
+          t.string('datatype', 255).notNull();
+          t.integer('position', 255).notNull();
+        });
+      }
     }),
   ]);
 };
@@ -42,6 +77,7 @@ exports.up = function(knex) {
 exports.down = function(knex) {
   return Promise.all([
     knex.schema.dropTable('columns'),
+    knex.schema.dropTable('headers'),
     knex.schema.dropTable('datasets'),
     knex.schema.dropTable('pipelines'),
     knex.schema.dropTable('experiments'),
