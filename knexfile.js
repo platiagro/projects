c5a35e81da33ba6998bea4ea60dc1f0498e5ad75
Miguel F Ferraz <39593684+miguelfferraz@users.noreply.github.com>
2019-10-14 09:58:20 -0300

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
 
-const password = process.env.DB_ROOT_PASSWORD || 'secret';
 const database = process.env.DB_DATABASE || 'projects';
 const host = process.env.DB_HOST || 'mysql';
 const port = process.env.DB_PORT || 3306;
 
-const testPassword = process.env.DB_TEST_ROOT_PASSWORD || 'secret';
 const testDatabase = process.env.DB_TEST_DATABASE || 'projects';
 const testHost = process.env.DB_TEST_HOST || 'mysql';
 const testPort = process.env.DB_TEST_PORT || 3306;
@@ -14,7 +12,6 @@ const config = {
   client: 'mysql',
   connection: {
     user: 'root',
-    password,
     host,
     port,
     database,
@@ -25,7 +22,6 @@ const testConfig = {
   client: 'mysql',
   connection: {
     user: 'root',
-    password: testPassword,
     database: testDatabase,
     host: testHost,
     port: testPort,
