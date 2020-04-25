a283454b2e8e23745ff141954ca807e8841c8833
Fábio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-04-25 17:30:55 -0300

    Corrects setup.py version (#41)
    See for furter explanations: https://stackoverflow.com/a/2073599

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
+import re
+from os.path import dirname, join
 from setuptools import find_packages, setup
 
-from projects import __version__
+with open(join(dirname(__file__), "projects", "__init__.py")) as fp:
+    for line in fp:
+        m = re.search(r'^\s*__version__\s*=\s*([\'"])([^\'"]+)\1\s*$', line)
+        if m:
+            version = m.group(2)
+            break
+    else:
+        raise RuntimeError("Unable to find own __version__ string")
 
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
@@ -16,7 +25,7 @@ extras = {
 
 setup(
     name="projects",
-    version=__version__,
+    version=version,
     author="Fabio Beranizo Lopes",
     author_email="fabio.beranizo@gmail.com",
     description="Manages projects.",
