686ca61206823b9c543686bd3075f77bc63b1c49
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-05-30 00:37:34 -0300

    Replaces to_json by to_dict
    - Flasks already does json_encoding, so return a dict.
    - Adds unit tests
    - Do not return pandas.DataFrame index

--- projects/controllers/datasets.py
+++ projects/controllers/datasets.py
@@ -3,9 +3,10 @@
 
 import platiagro
 
-from werkzeug.exceptions import BadRequest, NotFound
+from werkzeug.exceptions import NotFound
 
-from .utils import raise_if_project_does_not_exist
+from .utils import raise_if_operator_does_not_exist, \
+    raise_if_project_does_not_exist
 from ..models import Experiment
 
 
@@ -24,10 +25,15 @@ def get_dataset(project_id, experiment_id, operator_id):
     if experiment is None:
         raise NotFound("The specified experiment does not exist")
 
+    raise_if_operator_does_not_exist(operator_id)
+
     try:
         dataset = platiagro.load_dataset(name=experiment.dataset,
-                                         run_id='latest',
+                                         run_id="latest",
                                          operator_id=operator_id)
-        return dataset.to_json(orient='split')
-    except (FileNotFoundError) as e:
-        raise BadRequest(str(e))
+        dataset = dataset.to_dict(orient="split")
+        del dataset["index"]
+    except FileNotFoundError as e:
+        raise NotFound(str(e))
+
+    return dataset
