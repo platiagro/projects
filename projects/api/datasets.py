867d5f73c9db4649bdb91c900a65dcbcd1202690
Alende Geraldo <meubebe21@hotmail.com>
2020-07-01 09:20:21 -0300

    pagination datasets

--- projects/api/datasets.py
+++ projects/api/datasets.py
@@ -16,7 +16,7 @@ def handle_get_dataset(project_id, experiment_id, operator_id):
 
 @bp.route("/", methods=["GET"])
 @bp.paginate()
-def handle_get_dataset(project_id, experiment_id, operator_id, pagination_parameters):
+def handle_get_dataset_paginated(project_id, experiment_id, operator_id, pagination_parameters):
     """Handles GET requests to /."""
     datasets = get_dataset_pagination(project_id=project_id, experiment_id=experiment_id, operator_id=operator_id,
                        page=pagination_parameters.page, page_size=pagination_parameters.page_size)
