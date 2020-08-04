f9894af7213f32170fb6fdf4a45888f88697c1f9
Alende Geraldo <meubebe21@hotmail.com>
2020-08-04 13:45:57 -0300

    unify api

--- projects/api/datasets.py
+++ projects/api/datasets.py
@@ -8,12 +8,6 @@ from ..controllers.datasets import get_dataset, get_dataset_pagination
 bp = Blueprint("datasets", __name__)
 
 
-@bp.route("", methods=["GET"])
-def handle_get_dataset(project_id, experiment_id, operator_id):
-    """Handles GET requests to /."""
-    return get_dataset(project_id=project_id, experiment_id=experiment_id, operator_id=operator_id)
-
-
 @bp.route("/", methods=["GET"])
 @bp.paginate()
 def handle_get_dataset_paginated(project_id, experiment_id, operator_id, pagination_parameters):
