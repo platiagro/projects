5a94cf703fc9e73f9ae1a0d405c66406751dbb38
Alende Geraldo <meubebe21@hotmail.com>
2020-08-04 15:53:49 -0300

    unify api

--- projects/api/datasets.py
+++ projects/api/datasets.py
@@ -3,14 +3,14 @@
 
 from flask_smorest import Blueprint
 
-from ..controllers.datasets import get_dataset, get_dataset_pagination
+from ..controllers.datasets import get_dataset_pagination
 
 bp = Blueprint("datasets", __name__)
 
 
 @bp.route("/", methods=["GET"])
 @bp.paginate()
-def handle_get_dataset_paginated(project_id, experiment_id, operator_id, pagination_parameters):
+def handle_list_datasets(project_id, experiment_id, operator_id, pagination_parameters):
     """Handles GET requests to /."""
     datasets = get_dataset_pagination(project_id=project_id, experiment_id=experiment_id, operator_id=operator_id,
                                       page=pagination_parameters.page, page_size=pagination_parameters.page_size)
--- projects/controllers/datasets.py
+++ projects/controllers/datasets.py
@@ -9,43 +9,6 @@ from .utils import raise_if_experiment_does_not_exist, \
     raise_if_project_does_not_exist, pagination_datasets
 from ..models import Operator
 
-
-def get_dataset(project_id, experiment_id, operator_id):
-    """Retrieves a dataset as json.
-
-    Args:
-        project_id (str): the project uuid.
-        experiment_id (str): the experiment uuid.
-        operator_id (str): the operator uuid.
-    """
-    raise_if_project_does_not_exist(project_id)
-
-    raise_if_experiment_does_not_exist(experiment_id)
-
-    operator = Operator.query.get(operator_id)
-    if operator is None:
-        raise NotFound("The specified operator does not exist")
-
-    # get dataset name
-    dataset = operator.parameters.get('dataset')
-    if dataset is None:
-        raise NotFound()
-    try:
-        metadata = platiagro.stat_dataset(name=dataset, operator_id=operator_id)
-        if "run_id" not in metadata:
-            raise FileNotFoundError()
-
-        dataset = platiagro.load_dataset(name=dataset,
-                                         run_id="latest",
-                                         operator_id=operator_id)
-        dataset = dataset.to_dict(orient="split")
-        del dataset["index"]
-    except FileNotFoundError as e:
-        raise NotFound(str(e))
-
-    return dataset
-
-
 def get_dataset_pagination(project_id, experiment_id, operator_id, page, page_size):
     """Retrieves a dataset as json.
 
