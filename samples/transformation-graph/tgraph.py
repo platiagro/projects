88e5b3809d34aa9a6f04c2a06fc84f3e46459a35
lucaslzl <lznladeira@gmail.com>
2020-08-14 11:20:56 -0300

    Remove commented code and add import to pandas.

--- samples/transformation-graph/tgraph.py
+++ samples/transformation-graph/tgraph.py
@@ -21,7 +21,6 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import KFold, cross_val_score
-from sklearn.decomposition import PCA
 from sklearn.metrics import matthews_corrcoef, make_scorer
 
 
@@ -329,38 +328,6 @@ class TGraph:
                 solution['{0}---{1}'.format(transformation, self.date.name)] = new_feature
 
 
-    def apply_select(self, solution):
-        """
-        Apply PCA to select principal features.
-        Parameters
-        ----------
-        solution: pandas.DataFrame
-            A solution to be applied numeric transformations.
-        Returns
-        ----------
-        Nothing
-        """
-
-        solution = solution.fillna(method='ffill').fillna(method='bfill')
-
-        only_num = set(solution.columns).intersection(self.num_feats)
-
-        # Execute PCA to obtain a sub group of features
-        model = PCA(n_components=0.98).fit(solution[only_num])
-
-        # Obtain only the important features
-        num_components = model.components_.shape[0]
-        important_feats = [np.abs(model.components_[i]).argmax() for i in range(num_components)]
-
-        # Discover which features are
-        initial_feats = list(solution.columns)
-        selected_feats = [initial_feats[important_feats[i]] for i in range(num_components)]
-
-        selected_feats.extend(self.group_var)
-
-        solution = solution[list(set(selected_feats))]
-
-
     def apply_transformation(self, solution, transformation=None, trans_type=None):
         """
         Apply a transformation to a solution.
@@ -387,9 +354,6 @@ class TGraph:
         elif trans_type == 'time':
             self.apply_timely(solution, transformation)
 
-        # else:
-        #     self.apply_select(solution)
-
         return solution
 
 
@@ -413,12 +377,6 @@ class TGraph:
 
                 # Add solution to graph
                 self.add_to_graph(0, new_solution, trans)
-
-        # Apply the transformation
-        # new_solution = self.apply_transformation(self.G.nodes[0]['solution'].copy())
-
-        # Add solution to graph
-        # self.add_to_graph(0, new_solution, 'select')
 
 
     def search_best_node(self):
@@ -543,8 +501,6 @@ class TGraph:
                     trans_type = 'grouped'
                 elif trans in self.transformations['time']:
                     trans_type = 'time'
-                # else:
-                #     trans_type = 'select'
 
                 # Apply the transformation
                 new_solution = self.apply_transformation(self.G.nodes[node_id]['solution'].copy(), trans, trans_type)
