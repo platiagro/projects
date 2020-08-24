064b69e93704e510829de6903892ad278ef004e7
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2020-08-24 13:12:48 -0300

    Fix unit tests
    Don't reuse datasets (crates a new one after each test).

--- tests/components/test_preprocessors.py
+++ tests/components/test_preprocessors.py
@@ -1,6 +1,6 @@
-import os
-from os import environ, path, remove
+from os import environ, makedirs, path, remove
 from requests import get
+from unittest import TestCase
 
 from pytest import fixture
 from papermill import execute_notebook
@@ -26,56 +26,57 @@ BOSTON_DATASET = "boston.csv"
 BOSTON_TARGET = "medv"
 
 
-@fixture(scope="function", autouse=True)
-def setup(request):
-    # Set environment variables needed to run notebooks
-    environ["EXPERIMENT_ID"] = EXPERIMENT_ID
-    environ["OPERATOR_ID"] = OPERATOR_ID
-    environ["RUN_ID"] = RUN_ID
+class TestClassifiers(TestCase):
 
-    iris_content = \
-        get('https://raw.githubusercontent.com/platiagro/datasets/master/samples/iris.csv').content
+    def setUp(self):
+        # Set environment variables needed to run notebooks
+        environ["EXPERIMENT_ID"] = EXPERIMENT_ID
+        environ["OPERATOR_ID"] = OPERATOR_ID
+        environ["RUN_ID"] = RUN_ID
 
-    titanic_content = \
-        get('https://raw.githubusercontent.com/platiagro/datasets/master/samples/titanic.csv').content
+        iris_content = \
+            get("https://raw.githubusercontent.com/platiagro/datasets/master/samples/iris.csv").content
 
-    eucalyptus_content = \
-        get("https://raw.githubusercontent.com/platiagro/datasets/master/samples/eucalyptus.csv").content
+        titanic_content = \
+            get("https://raw.githubusercontent.com/platiagro/datasets/master/samples/titanic.csv").content
 
-    boston_content = \
-        get('https://raw.githubusercontent.com/platiagro/datasets/master/samples/boston.csv').content
+        eucalyptus_content = \
+            get("https://raw.githubusercontent.com/platiagro/datasets/master/samples/eucalyptus.csv").content
 
-    # Creates mock iris dataset
-    creates_mock_dataset(IRIS_DATASET, iris_content)
-    creates_iris_metadata(IRIS_DATASET)
+        boston_content = \
+            get("https://raw.githubusercontent.com/platiagro/datasets/master/samples/boston.csv").content
 
-    os.makedirs('/tmp/data', exist_ok=True)
+        # Creates mock iris dataset
+        creates_mock_dataset(IRIS_DATASET, iris_content)
+        creates_iris_metadata(IRIS_DATASET)
 
-    with open(f'/tmp/data/{IRIS_DATASET}', 'wb') as f:
-        f.write(iris_content)
+        makedirs("/tmp/data", exist_ok=True)
 
-    # Creates mock titanic dataset
-    creates_mock_dataset(TITANIC_DATASET, titanic_content)
-    creates_titanic_metadata(TITANIC_DATASET)
+        with open(f"/tmp/data/{IRIS_DATASET}", "wb") as f:
+            f.write(iris_content)
 
-    with open(f'/tmp/data/{TITANIC_DATASET}', 'wb') as f:
-        f.write(titanic_content)
+        # Creates mock titanic dataset
+        creates_mock_dataset(TITANIC_DATASET, titanic_content)
+        creates_titanic_metadata(TITANIC_DATASET)
 
-    # Creates mock eucalyptus dataset
-    creates_mock_dataset(EUCALYPTUS_DATASET, eucalyptus_content)
-    creates_eucalyptus_metadata(EUCALYPTUS_DATASET)
+        with open(f"/tmp/data/{TITANIC_DATASET}", "wb") as f:
+            f.write(titanic_content)
 
-    with open(f'/tmp/data/{EUCALYPTUS_DATASET}', 'wb') as f:
-        f.write(eucalyptus_content)
+        # Creates mock eucalyptus dataset
+        creates_mock_dataset(EUCALYPTUS_DATASET, eucalyptus_content)
+        creates_eucalyptus_metadata(EUCALYPTUS_DATASET)
 
-    # Creates mock boston dataset
-    creates_mock_dataset(BOSTON_DATASET, boston_content)
-    creates_boston_metadata(BOSTON_DATASET)
+        with open(f"/tmp/data/{EUCALYPTUS_DATASET}", "wb") as f:
+            f.write(eucalyptus_content)
 
-    with open(f'/tmp/data/{BOSTON_DATASET}', 'wb') as f:
-        f.write(boston_content)
+        # Creates mock boston dataset
+        creates_mock_dataset(BOSTON_DATASET, boston_content)
+        creates_boston_metadata(BOSTON_DATASET)
 
-    def delete_datasets():
+        with open(f"/tmp/data/{BOSTON_DATASET}", "wb") as f:
+            f.write(boston_content)
+
+    def tearDown(self):
         files_after_executed = ["CustomTransformer.py", "simulated.py", "tgraph.py",
                                 "Model.py", "contract.json"]
 
@@ -89,114 +90,114 @@ def setup(request):
         delete_mock_dataset(EUCALYPTUS_DATASET)
         delete_mock_dataset(BOSTON_DATASET)
 
-    request.addfinalizer(delete_datasets)
+    def test_filter_selection(self):
+        experiment_path = "samples/filter-selection/Experiment.ipynb"
+        deployment_path = "samples/filter-selection/Deployment.ipynb"
 
-# Teste OK
-def test_filter_selection(setup):
-    experiment_path = "samples/filter-selection/Experiment.ipynb"
-    deployment_path = "samples/filter-selection/Deployment.ipynb"
+        # Run test with iris and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET,
+                                                                       target=IRIS_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET,
+                                                                       features_to_filter=["PassengerId"]))
 
-    # Run test with iris and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET, target=IRIS_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
-                                                           target=TITANIC_TARGET,
-                                                           features_to_filter=["PassengerId"]))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_imputer(self):
+        experiment_path = "samples/imputer/Experiment.ipynb"
+        deployment_path = "samples/imputer/Deployment.ipynb"
 
-# Teste OK
-def test_imputer(setup):
-    experiment_path = "samples/imputer/Experiment.ipynb"
-    deployment_path = "samples/imputer/Deployment.ipynb"
+        # Run test with iris and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET,
+                                                                       target=IRIS_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET))
 
-    # Run test with iris and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET, target=IRIS_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET, target=TITANIC_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_normalizer(self):
+        experiment_path = "samples/normalizer/Experiment.ipynb"
+        deployment_path = "samples/normalizer/Deployment.ipynb"
 
-# Teste OK
-def test_normalizer(setup):
-    experiment_path = "samples/normalizer/Experiment.ipynb"
-    deployment_path = "samples/normalizer/Deployment.ipynb"
+        # Run test with iris and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET,
+                                                                       target=IRIS_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET))
 
-    # Run test with iris and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET, target=IRIS_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET, target=TITANIC_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_pre_selection(self):
+        experiment_path = "samples/pre-selection/Experiment.ipynb"
+        deployment_path = "samples/pre-selection/Deployment.ipynb"
 
-# Teste OK
-def test_pre_selection(setup):
-    experiment_path = "samples/pre-selection/Experiment.ipynb"
-    deployment_path = "samples/pre-selection/Deployment.ipynb"
+        # Run test with iris and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET,
+                                                                       target=IRIS_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET))
 
-    # Run test with iris and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET, target=IRIS_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET, target=TITANIC_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_robust_scaler(self):
+        experiment_path = "samples/robust-scaler/Experiment.ipynb"
+        deployment_path = "samples/robust-scaler/Deployment.ipynb"
 
-# Teste OK
-def test_robust_scaler(setup):
-    experiment_path = "samples/robust-scaler/Experiment.ipynb"
-    deployment_path = "samples/robust-scaler/Deployment.ipynb"
+        # Run test with iris and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET,
+                                                                       target=IRIS_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET))
 
-    # Run test with iris and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET, target=IRIS_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET, target=TITANIC_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_variance_threshold(self):
+        experiment_path = "samples/variance-threshold/Experiment.ipynb"
+        deployment_path = "samples/variance-threshold/Deployment.ipynb"
 
-# Teste OK
-def test_variance_threshold(setup):
-    experiment_path = "samples/variance-threshold/Experiment.ipynb"
-    deployment_path = "samples/variance-threshold/Deployment.ipynb"
+        # Run test with iris and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET,
+                                                                       target=IRIS_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET))
 
-    # Run test with iris and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=IRIS_DATASET, target=IRIS_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET, target=TITANIC_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_rfe_selector(self):
+        experiment_path = "samples/rfe-selector/Experiment.ipynb"
+        deployment_path = "samples/rfe-selector/Deployment.ipynb"
 
-# Teste OK 
-def test_rfe_selector(setup):
-    experiment_path = "samples/rfe-selector/Experiment.ipynb"
-    deployment_path = "samples/rfe-selector/Deployment.ipynb"
+        # Run test with boston dataset
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET,
+                                                                       target=BOSTON_TARGET))
 
-    # Run test with boston dataset
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET,
-                                                           target=BOSTON_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_simulated_annealing(self):
+        experiment_path = "samples/simulated-annealing/Experiment.ipynb"
+        deployment_path = "samples/simulated-annealing/Deployment.ipynb"
 
-# Erro no Notebook
-def test_simulated_annealing(setup):
-    experiment_path = "samples/simulated-annealing/Experiment.ipynb"
-    deployment_path = "samples/simulated-annealing/Deployment.ipynb"
+        # Run test with eucalyptus dataset
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=EUCALYPTUS_DATASET,
+                                                                       target=EUCALYPTUS_TARGET))
 
-    # Run test with eucalyptus dataset
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=EUCALYPTUS_DATASET,
-                                                           target=EUCALYPTUS_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_transformation_graph(self):
+        experiment_path = "samples/transformation-graph/Experiment.ipynb"
+        deployment_path = "samples/transformation-graph/Deployment.ipynb"
 
-# Erro no Notebook, Dependências adicionadas
-def test_transformation_graph(setup):
-    experiment_path = "samples/transformation-graph/Experiment.ipynb"
-    deployment_path = "samples/transformation-graph/Deployment.ipynb"
+        # Run test with eucalyptus dataset
+        execute_notebook(experiment_path, "/dev/null",
+                         parameters=dict(dataset=EUCALYPTUS_DATASET, target=EUCALYPTUS_TARGET))
 
-    # Run test with eucalyptus dataset
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=EUCALYPTUS_DATASET, target=EUCALYPTUS_TARGET))
-
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
- 
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
--- tests/components/test_regressors.py
+++ tests/components/test_regressors.py
@@ -1,6 +1,6 @@
-import os
-from os import environ, path, remove
+from os import environ, makedirs, path, remove
 from requests import get
+from unittest import TestCase
 
 from pytest import fixture
 from papermill import execute_notebook
@@ -20,36 +20,37 @@ TITANIC_DATASET = "titanic.csv"
 TITANIC_TARGET = "Fare"
 
 
-@fixture(scope="module", autouse=True)
-def setup(request):
-    # Set environment variables needed to run notebooks
-    environ["EXPERIMENT_ID"] = EXPERIMENT_ID
-    environ["OPERATOR_ID"] = OPERATOR_ID
-    environ["RUN_ID"] = RUN_ID
+class TestRegressors(TestCase):
 
-    boston_content = \
-        get('https://raw.githubusercontent.com/platiagro/datasets/master/samples/boston.csv').content
+    def setUp(self):
+        # Set environment variables needed to run notebooks
+        environ["EXPERIMENT_ID"] = EXPERIMENT_ID
+        environ["OPERATOR_ID"] = OPERATOR_ID
+        environ["RUN_ID"] = RUN_ID
 
-    titanic_content = \
-        get('https://raw.githubusercontent.com/platiagro/datasets/master/samples/titanic.csv').content
+        boston_content = \
+            get("https://raw.githubusercontent.com/platiagro/datasets/master/samples/boston.csv").content
 
-    # Creates mock boston dataset
-    creates_mock_dataset(BOSTON_DATASET, boston_content)
-    creates_boston_metadata(BOSTON_DATASET)
+        titanic_content = \
+            get("https://raw.githubusercontent.com/platiagro/datasets/master/samples/titanic.csv").content
 
-    os.makedirs('/tmp/data', exist_ok=True)
+        # Creates mock boston dataset
+        creates_mock_dataset(BOSTON_DATASET, boston_content)
+        creates_boston_metadata(BOSTON_DATASET)
 
-    with open(f'/tmp/data/{BOSTON_DATASET}', 'wb') as f:
-        f.write(boston_content)
+        makedirs("/tmp/data", exist_ok=True)
 
-    # Creates mock titanic dataset
-    creates_mock_dataset(TITANIC_DATASET, titanic_content)
-    creates_titanic_metadata(TITANIC_DATASET)
+        with open(f"/tmp/data/{BOSTON_DATASET}", "wb") as f:
+            f.write(boston_content)
 
-    with open(f'/tmp/data/{TITANIC_DATASET}', 'wb') as f:
-        f.write(titanic_content)
+        # Creates mock titanic dataset
+        creates_mock_dataset(TITANIC_DATASET, titanic_content)
+        creates_titanic_metadata(TITANIC_DATASET)
 
-    def delete_datasets():
+        with open(f"/tmp/data/{TITANIC_DATASET}", "wb") as f:
+            f.write(titanic_content)
+
+    def tearDown(self):
         files_after_executed = ["Model.py", "contract.json"]
 
         for generated_file in files_after_executed:
@@ -60,64 +61,67 @@ def setup(request):
         delete_mock_dataset(BOSTON_DATASET)
         delete_mock_dataset(TITANIC_DATASET)
 
-    request.addfinalizer(delete_datasets)
+    def test_automl_regressor(self):
+        experiment_path = "samples/automl-regressor/Experiment.ipynb"
+        deployment_path = "samples/automl-regressor/Deployment.ipynb"
 
-# Teste OK
-def test_automl_regressor(setup):
-    experiment_path = "samples/automl-regressor/Experiment.ipynb"
-    deployment_path = "samples/automl-regressor/Deployment.ipynb"
+        # Run test with boston and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET,
+                                                                       target=BOSTON_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET))
 
-    # Run test with boston and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET, target=BOSTON_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET, target=TITANIC_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_linear_regression(self):
+        experiment_path = "samples/linear-regression/Experiment.ipynb"
+        deployment_path = "samples/linear-regression/Deployment.ipynb"
 
-# Problema no Notebook
-def test_linear_regression(setup):
-    experiment_path = "samples/linear-regression/Experiment.ipynb"
-    deployment_path = "samples/linear-regression/Deployment.ipynb"
+        # Run test with boston and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET,
+                                                                       target=BOSTON_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET,
+                                                                       target=BOSTON_TARGET))
 
-    # Run test with boston and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET, target=BOSTON_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET, target=BOSTON_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_mlp_regressor(self):
+        experiment_path = "samples/mlp-regressor/Experiment.ipynb"
+        deployment_path = "samples/mlp-regressor/Deployment.ipynb"
 
-# Teste OK
-def test_mlp_regressor(setup):
-    experiment_path = "samples/mlp-regressor/Experiment.ipynb"
-    deployment_path = "samples/mlp-regressor/Deployment.ipynb"
+        # Run test with boston and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET,
+                                                                       target=BOSTON_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET))
 
-    # Run test with boston and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET, target=BOSTON_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET, target=TITANIC_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_random_forest_regressor(self):
+        experiment_path = "samples/random-forest-regressor/Experiment.ipynb"
+        deployment_path = "samples/random-forest-regressor/Deployment.ipynb"
 
-# Teste OK
-def test_random_forest_regressor(setup):
-    experiment_path = "samples/random-forest-regressor/Experiment.ipynb"
-    deployment_path = "samples/random-forest-regressor/Deployment.ipynb"
+        # Run test with boston and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET,
+                                                                       target=BOSTON_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET))
 
-    # Run test with boston and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET, target=BOSTON_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET, target=TITANIC_TARGET))
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
 
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+    def test_svr(self):
+        experiment_path = "samples/svr/Experiment.ipynb"
+        deployment_path = "samples/svr/Deployment.ipynb"
 
-# Teste OK
-def test_svr(setup):
-    experiment_path = "samples/svr/Experiment.ipynb"
-    deployment_path = "samples/svr/Deployment.ipynb"
+        # Run test with boston and titanic datasets
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET,
+                                                                       target=BOSTON_TARGET))
+        execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET,
+                                                                       target=TITANIC_TARGET))
 
-    # Run test with boston and titanic datasets
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=BOSTON_DATASET, target=BOSTON_TARGET))
-    execute_notebook(experiment_path, "/dev/null", parameters=dict(dataset=TITANIC_DATASET, target=TITANIC_TARGET))
-
-    # Deploy component
-    execute_notebook(deployment_path, "/dev/null")
+        # Deploy component
+        execute_notebook(deployment_path, "/dev/null")
