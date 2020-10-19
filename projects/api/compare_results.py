43feedcc10f4abc75f5d6e2074e4f9c37be25663
Ricardo Shiguemi Hiramatsu <ricardoh@aquarius.cpqd.com.br>
2020-10-19 10:16:51 -0300

    Fix compare result api method names

--- projects/api/compare_results.py
+++ projects/api/compare_results.py
@@ -11,29 +11,31 @@ bp = Blueprint("compare_results", __name__)
 
 
 @bp.route("", methods=["GET"])
-def handle_list_experiments(project_id):
+def handle_list_compare_results(project_id):
     """Handles GET requests to /."""
     return jsonify(list_compare_results(project_id=project_id))
 
 
 @bp.route("", methods=["POST"])
-def handle_post_experiments(project_id):
+def handle_post_compare_results(project_id):
     """Handles POST requests to /."""
     compare_result = create_compare_result(project_id=project_id)
     return jsonify(compare_result)
 
 
 @bp.route("<compare_result_id>", methods=["PATCH"])
-def handle_patch_experiment(project_id, compare_result_id):
+def handle_patch_compare_results(project_id, compare_result_id):
     """Handles PATCH requests to /<compare_result_id>."""
     kwargs = request.get_json(force=True)
     kwargs = {to_snake_case(k): v for k, v in kwargs.items()}
-    experiment = update_compare_result(uuid=compare_result_id, project_id=project_id, **kwargs)
-    return jsonify(experiment)
+    compare_result = update_compare_result(uuid=compare_result_id,
+                                           project_id=project_id,
+                                           **kwargs)
+    return jsonify(compare_result)
 
 
 @bp.route("<compare_result_id>", methods=["DELETE"])
-def handle_delete_experiment(project_id, compare_result_id):
+def handle_delete_compare_results(project_id, compare_result_id):
     """Handles DELETE requests to /<compare_result_id>."""
-    experiment = delete_compare_result(uuid=compare_result_id, project_id=project_id)
-    return jsonify(experiment)
+    compare_result = delete_compare_result(uuid=compare_result_id, project_id=project_id)
+    return jsonify(compare_result)
