da581444b3b156bde364f40708f3ed011f77dc8a
Miguel Figueira Ferraz <mferraz@mcity76.aquarius.cpqd.com.br>
2021-01-19 15:09:24 -0300

    Add Parameters controller - Create folder for Operators controller, its includes Parameters Controller

---
+++ projects/controllers/operators/__init__.py
@@ -0,0 +1,8 @@
+from .operators import list_operators, create_operator, update_operator, delete_operator
+
+__all__ = [
+    "list_operators",
+    "create_operator",
+    "update_operator",
+    "delete_operator",
+]
