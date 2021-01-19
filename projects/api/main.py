5d902b6c4a400731b61dc9569a71dc35db8f48f7
Miguel Figueira Ferraz <mferraz@mcity76.aquarius.cpqd.com.br>
2021-01-19 15:05:10 -0300

    Add Parameters API - Reorganize Operators API structure - Include /projects/:id/experiments/:id/operators/:id/parameters/:name endpoint - Create folder for Operators API, its includes Parameters API

--- projects/api/main.py
+++ projects/api/main.py
@@ -3,44 +3,114 @@
 import argparse
 import sys
 
-import uvicorn
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse, PlainTextResponse
+from flask import Flask, jsonify
+from flask_cors import CORS
+from werkzeug.exceptions import BadRequest, NotFound, MethodNotAllowed, \
+    Forbidden, InternalServerError
 
-from projects.api import comparisons, deployments, experiments, monitorings, \
-    predictions, projects, tasks, templates
-from projects.api.deployments import runs as deployment_runs
-from projects.api.deployments.runs import logs as deployment_logs
-from projects.api.experiments import operators, runs as experiment_runs
-from projects.api.experiments.runs import datasets, figures, \
-    logs as experiment_logs, metrics
-from projects.api.tasks import parameters
-from projects.database import engine, init_db
-from projects.exceptions import BadRequest, NotFound, InternalServerError
+from projects.api.comparisons import bp as comparisons_blueprint
+from projects.api.deployments import bp as deployments_blueprint
+from projects.api.deployments.runs import bp as deployments_runs_blueprint
+from projects.api.deployments.runs.logs import bp as deployments_logs_blueprint
+from projects.api.experiments import bp as experiments_blueprint
+from projects.api.experiments.operators import bp as experiments_operators_blueprint
+from projects.api.experiments.operators.parameters import bp as operators_parameters_blueprint
+from projects.api.experiments.runs import bp as experiments_runs_blueprint
+from projects.api.experiments.runs.datasets import bp as datasets_blueprint
+from projects.api.experiments.runs.figures import bp as figures_blueprint
+from projects.api.experiments.runs.logs import bp as experiments_logs_blueprint
+from projects.api.experiments.runs.metrics import bp as metrics_blueprint
+from projects.api.json_encoder import CustomJSONEncoder
+from projects.api.monitorings import bp as monitorings_blueprint
+from projects.api.predictions import bp as predictions_blueprint
+from projects.api.projects import bp as projects_blueprint
+from projects.api.tasks import bp as tasks_blueprint
+from projects.api.tasks.parameters import bp as tasks_parameters_blueprint
+from projects.api.templates import bp as templates_blueprint
+from projects.database import db_session, init_db
+
+app = Flask(__name__)
+app.json_encoder = CustomJSONEncoder
+app.register_blueprint(
+    projects_blueprint,
+    url_prefix="/projects",
+)
+app.register_blueprint(
+    comparisons_blueprint,
+    url_prefix="/projects/<project_id>/comparisons",
+)
+app.register_blueprint(
+    experiments_blueprint,
+    url_prefix="/projects/<project_id>/experiments",
+)
+app.register_blueprint(
+    experiments_operators_blueprint,
+    url_prefix="/projects/<project_id>/experiments/<experiment_id>/operators",
+)
+app.register_blueprint(
+    operators_parameters_blueprint,
+    url_prefix="/projects/<project_id>/experiments/<experiment_id>/operators/<operator_id>/parameters",
+)
+app.register_blueprint(
+    experiments_runs_blueprint,
+    url_prefix="/projects/<project_id>/experiments/<experiment_id>/runs",
+)
+app.register_blueprint(
+    datasets_blueprint,
+    url_prefix="/projects/<project_id>/experiments/<experiment_id>/runs/<run_id>/operators/<operator_id>/datasets",
+)
+app.register_blueprint(
+    figures_blueprint,
+    url_prefix="/projects/<project_id>/experiments/<experiment_id>/runs/<run_id>/operators/<operator_id>/figures",
+)
+app.register_blueprint(
+    experiments_logs_blueprint,
+    url_prefix="/projects/<project_id>/experiments/<experiment_id>/runs/<run_id>/operators/<operator_id>/logs",
+)
+app.register_blueprint(
+    metrics_blueprint,
+    url_prefix="/projects/<project_id>/experiments/<experiment_id>/runs/<run_id>/operators/<operator_id>/metrics",
+)
+app.register_blueprint(
+    deployments_blueprint,
+    url_prefix="/projects/<project_id>/deployments",
+)
+app.register_blueprint(
+    deployments_runs_blueprint,
+    url_prefix="/projects/<project_id>/deployments/<deployment_id>/runs",
+)
+app.register_blueprint(
+    deployments_logs_blueprint,
+    url_prefix="/projects/<project_id>/deployments/<deployment_id>/runs/<run_id>/logs",
+)
+app.register_blueprint(
+    monitorings_blueprint,
+    url_prefix="/projects/<project_id>/deployments/<deployment_id>/monitorings"
+)
+app.register_blueprint(
+    predictions_blueprint,
+    url_prefix="/projects/<project_id>/deployments/<deployment_id>/predictions",
+)
+app.register_blueprint(
+    tasks_blueprint,
+    url_prefix="/tasks",
+)
+app.register_blueprint(
+    tasks_parameters_blueprint,
+    url_prefix="/tasks/<task_id>/parameters",
+)
+app.register_blueprint(
+    templates_blueprint,
+    url_prefix="/templates",
+)
 
 
-app = FastAPI()
-app.include_router(projects.router)
-app.include_router(comparisons.router)
-app.include_router(experiments.router)
-app.include_router(operators.router)
-app.include_router(experiment_runs.router)
-app.include_router(datasets.router)
-app.include_router(figures.router)
-app.include_router(experiment_logs.router)
-app.include_router(metrics.router)
-app.include_router(deployments.router)
-app.include_router(deployment_runs.router)
-app.include_router(deployment_logs.router)
-app.include_router(monitorings.router)
-app.include_router(predictions.router)
-app.include_router(tasks.router)
-app.include_router(parameters.router)
-app.include_router(templates.router)
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
 
-@app.get("/", response_class=PlainTextResponse)
+@app.route("/", methods=["GET"])
 def ping():
     """
     Handles GET requests to /.
