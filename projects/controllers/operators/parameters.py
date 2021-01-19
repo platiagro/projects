3f0969b4396f286104cc17234e1b62fb7a8ce58a
Miguel Figueira Ferraz <mferraz@mcity76.aquarius.cpqd.com.br>
2021-01-19 15:09:24 -0300

    Add Parameters controller - Create folder for Operators controller, its includes Parameters Controller

---
+++ projects/controllers/operators/parameters.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Parameter controller."""
+
+
+def update_parameter(name, **kwargs):
+    """
+    Updatades a parameter from operator.
+
+    Parameters
+    ----------
+    project_id : str
+    experiment_id : str
+    operator_id : str
+    name : str
+    **kwargs
+        Arbitrary keyword arguments.
+
+    Returns
+    -------
+    dict
+        The parameter attributes.
+
+    Raises
+    ------
+    NotFound
+        When any of project_id, experiment_id, operator_id or name does not exist.
+    """
+    pass
