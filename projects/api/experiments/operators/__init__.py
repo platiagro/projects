8dc608470a35c011e9353a70e7fbfe62383b6cf7
Miguel Figueira Ferraz <mferraz@mcity76.aquarius.cpqd.com.br>
2021-01-21 16:33:39 -0300

    Rebase with master

---
+++ projects/api/experiments/operators/__init__.py
@@ -0,0 +1,10 @@
+from .operators import bp, handle_list_operators, handle_post_operator, \
+    handle_patch_operator, handle_delete_operator
+
+__all__ = [
+    "bp",
+    "handle_list_operators",
+    "handle_post_operator",
+    "handle_patch_operator",
+    "handle_delete_operator",
+]
