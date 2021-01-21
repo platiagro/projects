e929b7ae98ac5b5a92d0d1719e40ef04bfb64f82
Miguel Figueira Ferraz <mferraz@mcity76.aquarius.cpqd.com.br>
2021-01-21 14:37:26 -0300

    Add update_parameter controller

--- projects/api/experiments/operators/parameters.py
+++ projects/api/experiments/operators/parameters.py
@@ -5,11 +5,11 @@ from flask import Blueprint, jsonify, request
 from projects.controllers.operators.parameters import update_parameter
 from projects.utils import to_snake_case
 
-bp = Blueprint("parameters", __name__)
+bp = Blueprint("operator_parameters", __name__)
 
 
 @bp.route("<name>", methods=["PATCH"])
-def handle_patch_parameter(name):
+def handle_patch_parameter(project_id, experiment_id, operator_id, name):
     """
     Handles PATCH requests to /<name>.
 
@@ -20,5 +20,11 @@ def handle_patch_parameter(name):
     """
     kwargs = request.get_json(force=True)
     kwargs = {to_snake_case(k): v for k, v in kwargs.items()}
-    parameter = update_parameter(name=name, **kwargs)
+    parameter = update_parameter(
+        project_id=project_id,
+        experiment_id=experiment_id,
+        operator_id=operator_id,
+        name=name,
+        **kwargs
+    )
     return jsonify(parameter)
