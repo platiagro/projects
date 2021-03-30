da1a1f715e0074944080eb4e0010d780fbc4cbea
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2021-03-30 14:31:17 -0300

    Adds endpoint GET ..deployments/:id/operators
    Lists the operators of a deployment.

--- projects/api/deployments/operators/operators.py
+++ projects/api/deployments/operators/operators.py
@@ -13,6 +13,35 @@ router = APIRouter(
 )
 
 
+@router.get("", response_model=projects.schemas.operator.OperatorList)
+async def handle_list_operators(project_id: str,
+                                deployment_id: str,
+                                session: Session = Depends(session_scope)):
+    """
+    Handles GET requests to /.
+
+    Parameters
+    ----------
+    project_id : str
+    deployment_id : str
+    session : sqlalchemy.orm.session.Session
+
+    Returns
+    -------
+    projects.schemas.operator.OperatorList
+    """
+    project_controller = ProjectController(session)
+    project_controller.raise_if_project_does_not_exist(project_id)
+
+    deployment_controller = DeploymentController(session)
+    deployment_controller.raise_if_deployment_does_not_exist(deployment_id)
+
+    operator_controller = OperatorController(session)
+    operators = operator_controller.list_operators(project_id=project_id,
+                                                   deployment_id=deployment_id)
+    return operators
+
+
 @router.patch("/{operator_id}", response_model=projects.schemas.operator.Operator)
 async def handle_patch_operator(project_id: str,
                                 deployment_id: str,
