55dd27beaac72df877c2487a154fb205582c3580
Miguel Ferraz <miguel.f.ferraz.27@gmail.com>
2021-05-06 02:21:04 -0300

    Accept json on predictions API Now it is possible to send both json and form-data to predictions API. Sending a JSON with dataset name, the controller will load the dataset and parse it to the seldon's request format.

--- projects/controllers/predictions.py
+++ projects/controllers/predictions.py
@@ -3,9 +3,11 @@
 import json
 
 import requests
-from projects.exceptions import InternalServerError
+from platiagro import load_dataset
 
-from projects.controllers.utils import parse_file_buffer_to_seldon_request
+from projects.controllers.utils import parse_dataframe_to_seldon_request, \
+    parse_file_buffer_to_seldon_request
+from projects.exceptions import BadRequest, InternalServerError
 from projects.kubernetes.seldon import get_seldon_deployment_url
 
 
@@ -13,7 +15,7 @@ class PredictionController:
     def __init__(self, session):
         self.session = session
 
-    def create_prediction(self, project_id: str, deployment_id: str, file: bytes = None):
+    def create_prediction(self, project_id: str, deployment_id: str, upload_file: bytes = None, dataset: str = None):
         """
         POST a prediction file to seldon deployment.
 
@@ -21,15 +23,25 @@ class PredictionController:
         ----------
         project_id : str
         deployment_id : str
-        file : bytes
+        upload_file : starlette.datastructures.UploadFile
             File buffer.
+        dataset : str
+            Dataset name.
 
         Returns
         -------
         dict
         """
-        url = get_seldon_deployment_url(deployment_id, external_url=False)
-        request = parse_file_buffer_to_seldon_request(file)
+        if upload_file is not None:
+            file = upload_file.file
+            request = parse_file_buffer_to_seldon_request(file=file)
+        elif dataset is not None:
+            dataframe = load_dataset(dataset)
+            request = parse_dataframe_to_seldon_request(dataframe=dataframe)
+        else:
+            raise BadRequest("either dataset name or file is required")
+
+        url = get_seldon_deployment_url(deployment_id=deployment_id, external_url=False)
         response = requests.post(url, json=request)
 
         try:
