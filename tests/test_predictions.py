bd0effb9317b959a23067e9080fe036a4d377a15
Miguel Ferraz <miguel.f.ferraz.27@gmail.com>
2021-05-10 11:49:30 -0300

    Add unit test for image dataset

--- tests/test_predictions.py
+++ tests/test_predictions.py
@@ -1,4 +1,5 @@
 # -*-  coding: utf-8 -*-
+from base64 import b64decode
 from io import BytesIO
 from json import dumps
 from unittest import TestCase
@@ -26,11 +27,13 @@ POSITION = 0
 STATUS = "Pending"
 URL = None
 DATASET = "mock.csv"
+IMAGE_DATASET = "mock.jpg"
 
 MOCKED_DATASET_PATH = "tests/resources/mocked_dataset.csv"
 MOCKED_DATASET_NO_HEADER_PATH = "tests/resources/mocked_dataset_no_header.csv"
 MOCKED_DATASET_BASE64_PATH = "tests/resources/mocked_dataset_base64.jpeg"
 MOCKED_DATASET_STRDATA_PATH = "tests/resources/mocked_dataset_strdata.txt"
+MOCK_IMAGE = b64decode("R0lGODlhPQBEAPeoAJosM//AwO/AwHVYZ/z595kzAP/s7P+goOXMv8+fhw/v739/f+8PD98fH/8mJl+fn/9ZWb8/PzWlwv///6wWGbImAPgTEMImIN9gUFCEm/gDALULDN8PAD6atYdCTX9gUNKlj8wZAKUsAOzZz+UMAOsJAP/Z2ccMDA8PD/95eX5NWvsJCOVNQPtfX/8zM8+QePLl38MGBr8JCP+zs9myn/8GBqwpAP/GxgwJCPny78lzYLgjAJ8vAP9fX/+MjMUcAN8zM/9wcM8ZGcATEL+QePdZWf/29uc/P9cmJu9MTDImIN+/r7+/vz8/P8VNQGNugV8AAF9fX8swMNgTAFlDOICAgPNSUnNWSMQ5MBAQEJE3QPIGAM9AQMqGcG9vb6MhJsEdGM8vLx8fH98AANIWAMuQeL8fABkTEPPQ0OM5OSYdGFl5jo+Pj/+pqcsTE78wMFNGQLYmID4dGPvd3UBAQJmTkP+8vH9QUK+vr8ZWSHpzcJMmILdwcLOGcHRQUHxwcK9PT9DQ0O/v70w5MLypoG8wKOuwsP/g4P/Q0IcwKEswKMl8aJ9fX2xjdOtGRs/Pz+Dg4GImIP8gIH0sKEAwKKmTiKZ8aB/f39Wsl+LFt8dgUE9PT5x5aHBwcP+AgP+WltdgYMyZfyywz78AAAAAAAD///8AAP9mZv///wAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACH5BAEAAKgALAAAAAA9AEQAAAj/AFEJHEiwoMGDCBMqXMiwocAbBww4nEhxoYkUpzJGrMixogkfGUNqlNixJEIDB0SqHGmyJSojM1bKZOmyop0gM3Oe2liTISKMOoPy7GnwY9CjIYcSRYm0aVKSLmE6nfq05QycVLPuhDrxBlCtYJUqNAq2bNWEBj6ZXRuyxZyDRtqwnXvkhACDV+euTeJm1Ki7A73qNWtFiF+/gA95Gly2CJLDhwEHMOUAAuOpLYDEgBxZ4GRTlC1fDnpkM+fOqD6DDj1aZpITp0dtGCDhr+fVuCu3zlg49ijaokTZTo27uG7Gjn2P+hI8+PDPERoUB318bWbfAJ5sUNFcuGRTYUqV/3ogfXp1rWlMc6awJjiAAd2fm4ogXjz56aypOoIde4OE5u/F9x199dlXnnGiHZWEYbGpsAEA3QXYnHwEFliKAgswgJ8LPeiUXGwedCAKABACCN+EA1pYIIYaFlcDhytd51sGAJbo3onOpajiihlO92KHGaUXGwWjUBChjSPiWJuOO/LYIm4v1tXfE6J4gCSJEZ7YgRYUNrkji9P55sF/ogxw5ZkSqIDaZBV6aSGYq/lGZplndkckZ98xoICbTcIJGQAZcNmdmUc210hs35nCyJ58fgmIKX5RQGOZowxaZwYA+JaoKQwswGijBV4C6SiTUmpphMspJx9unX4KaimjDv9aaXOEBteBqmuuxgEHoLX6Kqx+yXqqBANsgCtit4FWQAEkrNbpq7HSOmtwag5w57GrmlJBASEU18ADjUYb3ADTinIttsgSB1oJFfA63bduimuqKB1keqwUhoCSK374wbujvOSu4QG6UvxBRydcpKsav++Ca6G8A6Pr1x2kVMyHwsVxUALDq/krnrhPSOzXG1lUTIoffqGR7Goi2MAxbv6O2kEG56I7CSlRsEFKFVyovDJoIRTg7sugNRDGqCJzJgcKE0ywc0ELm6KBCCJo8DIPFeCWNGcyqNFE06ToAfV0HBRgxsvLThHn1oddQMrXj5DyAQgjEHSAJMWZwS3HPxT/QMbabI/iBCliMLEJKX2EEkomBAUCxRi42VDADxyTYDVogV+wSChqmKxEKCDAYFDFj4OmwbY7bDGdBhtrnTQYOigeChUmc1K3QTnAUfEgGFgAWt88hKA6aCRIXhxnQ1yg3BCayK44EWdkUQcBByEQChFXfCB776aQsG0BIlQgQgE8qO26X1h8cEUep8ngRBnOy74E9QgRgEAC8SvOfQkh7FDBDmS43PmGoIiKUUEGkMEC/PJHgxw0xH74yx/3XnaYRJgMB8obxQW6kL9QYEJ0FIFgByfIL7/IQAlvQwEpnAC7DtLNJCKUoO/w45c44GwCXiAFB/OXAATQryUxdN4LfFiwgjCNYg+kYMIEFkCKDs6PKAIJouyGWMS1FSKJOMRB/BoIxYJIUXFUxNwoIkEKPAgCBZSQHQ1A2EWDfDEUVLyADj5AChSIQW6gu10bE/JG2VnCZGfo4R4d0sdQoBAHhPjhIB94v/wRoRKQWGRHgrhGSQJxCS+0pCZbEhAAOw==")
 
 
 class TestPredictions(TestCase):
@@ -60,12 +63,12 @@ class TestPredictions(TestCase):
 
         conn.close()
 
-        # uploads mock dataset
         try:
             MINIO_CLIENT.make_bucket(BUCKET_NAME)
         except BucketAlreadyOwnedByYou:
             pass
 
+        # uploads mock dataset
         file = BytesIO((
             b'col0,col1,col2,col3,col4,col5\n'
             b'01/01/2000,5.1,3.5,1.4,0.2,Iris-setosa\n'
@@ -91,6 +94,27 @@ class TestPredictions(TestCase):
             length=buffer.getbuffer().nbytes,
         )
 
+        # uploads png as dataset
+        file = BytesIO((
+            MOCK_IMAGE
+        ))
+        MINIO_CLIENT.put_object(
+            bucket_name=BUCKET_NAME,
+            object_name=f"datasets/{IMAGE_DATASET}/{IMAGE_DATASET}",
+            data=file,
+            length=file.getbuffer().nbytes,
+        )
+        metadata = {
+            "filename": IMAGE_DATASET,
+        }
+        buffer = BytesIO(dumps(metadata).encode())
+        MINIO_CLIENT.put_object(
+            bucket_name=BUCKET_NAME,
+            object_name=f"datasets/{IMAGE_DATASET}/{IMAGE_DATASET}.metadata",
+            data=buffer,
+            length=buffer.getbuffer().nbytes,
+        )
+
     @classmethod
     def tearDownClass(cls):
         MINIO_CLIENT.remove_object(
@@ -100,6 +124,14 @@ class TestPredictions(TestCase):
         MINIO_CLIENT.remove_object(
             bucket_name=BUCKET_NAME,
             object_name=f"datasets/{DATASET}/{DATASET}",
+        )
+        MINIO_CLIENT.remove_object(
+            bucket_name=BUCKET_NAME,
+            object_name=f"datasets/{IMAGE_DATASET}/{IMAGE_DATASET}.metadata",
+        )
+        MINIO_CLIENT.remove_object(
+            bucket_name=BUCKET_NAME,
+            object_name=f"datasets/{IMAGE_DATASET}/{IMAGE_DATASET}",
         )
 
         conn = engine.connect()
@@ -147,29 +179,30 @@ class TestPredictions(TestCase):
         self.assertEqual(result, expected)
         self.assertEqual(rv.status_code, 400)
 
-        # successful load dataset request
         # building a functional response for mocked post
         mocked_response = Response()
         mocked_response.status_code = 200
         mocked_response._content = b'{ "foo": "bar" }'
         mock_requests.post.return_value = mocked_response
-
+        
+        # successful load dataset request
         rv = TEST_CLIENT.post(
             f"/projects/{PROJECT_ID}/deployments/{DEPLOYMENT_ID}/predictions", 
             json={"dataset": DATASET}
         )
         result = rv.json()
+        self.assertIsInstance(result, dict)
+        self.assertEqual(rv.status_code, 200)
+
+        rv = TEST_CLIENT.post(
+           f"/projects/{PROJECT_ID}/deployments/{DEPLOYMENT_ID}/predictions", 
+            json={"dataset": IMAGE_DATASET}
+        )
         result = rv.json()
         self.assertIsInstance(result, dict)
         self.assertEqual(rv.status_code, 200)
 
         # successful csv request
-        # building a functional response for mocked post
-        mocked_response = Response()
-        mocked_response.status_code = 200
-        mocked_response._content = b'{ "foo": "bar" }'
-        mock_requests.post.return_value = mocked_response
-
         # reading file for request
         mocked_dataset = open(MOCKED_DATASET_PATH, "rb")
         files = {"file": (
@@ -187,12 +220,6 @@ class TestPredictions(TestCase):
         self.assertEqual(rv.status_code, 200)
 
         # successful base64 request
-        # building a functional response for mocked post
-        mocked_response = Response()
-        mocked_response.status_code = 200
-        mocked_response._content = b'{ "foo": "bar" }'
-        mock_requests.post.return_value = mocked_response
-
         # reading file for request
         mocked_dataset = open(MOCKED_DATASET_BASE64_PATH, "rb")
         files = {"file": (
@@ -210,12 +237,6 @@ class TestPredictions(TestCase):
         self.assertEqual(rv.status_code, 200)
 
         # successful strData request
-        # building a functional response for mocked post
-        mocked_response = Response()
-        mocked_response.status_code = 200
-        mocked_response._content = b'{ "foo": "bar" }'
-        mock_requests.post.return_value = mocked_response
-
         # reading file for request
         mocked_dataset = open(MOCKED_DATASET_STRDATA_PATH, "rb")
         files = {"file": (
