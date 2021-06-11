871e90e141ed66735b6b2e65f12198c82cc8635a
Andre Luiz Ferreira da Silva Nogueira <aluizfs@avelar.aquarius.cpqd.com.br>
2021-06-11 15:34:56 -0300

    EmailSchema now is using env vars, no more safety risk! Some try except snippets added

--- projects/schemas/mailing.py
+++ projects/schemas/mailing.py
@@ -1,17 +1,28 @@
+import os
+import warnings
+
 from pydantic import EmailStr, BaseModel
 from fastapi_mail import ConnectionConfig
 from typing import List
 
+MAIL_USERNAME = os.getenv("MAIL_USERNAME", "")
+MAIL_PASSWORD = os.getenv("MAIL_USERNAME", "")
+MAIL_SENDER_ADDRESS = os.getenv("MAIL_SENDER_ADDRESS", "")
+MAIL_SERVER = os.getenv("MAIL_USERNAME", "")
 
 class EmailSchema(BaseModel):
     email: List[EmailStr]
-
-    conf = ConnectionConfig(
-        MAIL_USERNAME="",
-        MAIL_PASSWORD="",
-        MAIL_FROM="",
-        MAIL_PORT=587,
-        MAIL_SERVER="smtp.mailgun.org",
-        MAIL_TLS=True,
-        MAIL_SSL=False
-    )
+    
+    try:
+        conf = ConnectionConfig(
+            MAIL_USERNAME=MAIL_USERNAME,
+            MAIL_PASSWORD=MAIL_PASSWORD,
+            MAIL_FROM=MAIL_SENDER_ADDRESS,
+            MAIL_PORT=587,
+            MAIL_SERVER=MAIL_SERVER,
+            MAIL_TLS=True,
+            MAIL_SSL=False
+        )
+    except Exception as e:
+        warnings.warn(f"Error while trying to config email schema:{e}")
+        
