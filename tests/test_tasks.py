810e1a96752b81a85eda551379a7b1c1d29bc4f3
Andre Luiz Ferreira da Silva Nogueira <aluizfs@avelar.aquarius.cpqd.com.br>
2021-06-15 14:14:28 -0300

    Putting env var in ci.yml Changing receiver email address in test

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -49,7 +49,7 @@ POSITION_Y = 0
 DEPENDENCIES_OP_ID = [OPERATOR_ID]
 DEPENDENCIES_OP_ID_JSON = dumps(DEPENDENCIES_OP_ID)
 
-EMAILS_TO_SEND = ["some.fictional.email.107554@gmail.com"]
+EMAILS_TO_SEND = ["some.fictional.email.107554@maildrop.cc"]
 
 
 class TestTasks(TestCase):
