e99d3fd0af571bae9da3c714375be531c79bdee3
Fabio Beranizo <fabiol@cpqd.com.br>
2021-06-24 18:05:43 -0300

    Adds a unittest for CORS OPTIONS preflight request
    Fix api options unit tests
    Removes --enable-cors references, adds --host references

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import os
+os.environ["ENABLE_CORS"] = "1"
 from unittest import TestCase
 
 from fastapi.testclient import TestClient
@@ -12,12 +14,14 @@ class TestApi(TestCase):
 
     def test_parse_args(self):
         parser = parse_args([])
+        self.assertEqual(parser.host, "127.0.0.1")
         self.assertEqual(parser.port, 8080)
-        self.assertFalse(parser.enable_cors)
 
-        parser = parse_args(["--enable-cors", "--port", "3000"])
+        parser = parse_args(["--host", "0.0.0.0"])
+        self.assertEqual(parser.host, "0.0.0.0")
+
+        parser = parse_args(["--port", "3000"])
         self.assertEqual(parser.port, 3000)
-        self.assertTrue(parser.enable_cors)
 
     def test_ping(self):
         rv = TEST_CLIENT.get("/")
@@ -25,3 +29,13 @@ class TestApi(TestCase):
         expected = "pong"
         self.assertEqual(result, expected)
         self.assertEqual(rv.status_code, 200)
+        self.assertTrue("Access-Control-Allow-Origin" in rv.headers)
+        self.assertTrue("Access-Control-Allow-Methods" in rv.headers)
+        self.assertTrue("Access-Control-Allow-Headers" in rv.headers)
+
+    def test_options_preflight(self):
+        rv = TEST_CLIENT.options("/")
+        self.assertEqual(rv.status_code, 200)
+        self.assertTrue("Access-Control-Allow-Origin" in rv.headers)
+        self.assertTrue("Access-Control-Allow-Methods" in rv.headers)
+        self.assertTrue("Access-Control-Allow-Headers" in rv.headers)
