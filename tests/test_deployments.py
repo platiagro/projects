a44d17daf2b0c658b8cf3e713b532d6c34445d11
Andre Luiz Ferreira da Silva Nogueira <aluizfs@avelar.aquarius.cpqd.com.br>
2021-08-03 09:42:55 -0300

    removing print

--- tests/test_deployments.py
+++ tests/test_deployments.py
@@ -702,7 +702,6 @@ class TestDeployments(TestCase):
     def test_get_deployment(self):
         rv = TEST_CLIENT.get(f"/projects/foo/deployments/{DEPLOYMENT_ID}")
         result = rv.json()
-        print(result)
         self.assertEqual(rv.status_code, 404)
 
         rv = TEST_CLIENT.get(f"/projects/foo/deployments/foo-bar")
