3b7c45e1c946f1b300d3e0aab952e8158fc32c6b
Fabio Beranizo <fabiol@cpqd.com.br>
2021-08-23 16:23:44 -0300

    Moves mailing configuration to projects/share_task package
    Moves it to the appropriate location.

--- projects/schemas/__init__.py
+++ projects/schemas/__init__.py
@@ -1,15 +1,15 @@
-from .comparison import Comparison, ComparisonList, ComparisonUpdate
+from .comparison import Comparison, ComparisonList, ComparisonUpdate  # noqa: F401
 from .deployment import Deployment, DeploymentCreate, DeploymentList, \
-    DeploymentUpdate
+    DeploymentUpdate  # noqa: F401
 from .experiment import Experiment, ExperimentCreate, ExperimentList, \
-    ExperimentUpdate
-from .log import Log, LogList
-from .message import Message
+    ExperimentUpdate  # noqa: F401
+from .log import Log, LogList  # noqa: F401
+from .message import Message  # noqa: F401
 from .monitoring import Monitoring, MonitoringCreate, MonitoringList, \
-    MonitoringUpdate
-from .operator import Operator, OperatorCreate, OperatorList, OperatorUpdate, Parameter
-from .project import Project, ProjectCreate, ProjectList, ProjectUpdate
-from .run import Run, RunList
-from .task import Task, TaskCreate, TaskList, TaskUpdate
+    MonitoringUpdate  # noqa: F401
+from .operator import Operator, OperatorCreate, OperatorList, OperatorUpdate, Parameter  # noqa: F401
+from .project import Project, ProjectCreate, ProjectList, ProjectUpdate  # noqa: F401
+from .run import Run, RunList  # noqa: F401
+from .task import Task, TaskCreate, TaskList, TaskUpdate  # noqa: F401
 from .template import Template, TemplateCreate, TemplateList, \
-    TemplateUpdate
+    TemplateUpdate  # noqa: F401
