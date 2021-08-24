2dfe16f5f49cc6234d00e3aba9bd7286b18ae036
Fabio Beranizo <fabiol@cpqd.com.br>
2021-08-24 11:16:06 -0300

    Adds 'OutOfpods' to IGNORABLE_STATUSES_REASONS
    Prevents the logs API to return an error.

--- projects/kubernetes/utils.py
+++ projects/kubernetes/utils.py
@@ -13,7 +13,7 @@ from projects.kubernetes.kube_config import load_kube_config
 
 IGNORABLE_MESSAGES_KEYTEXTS = ["ContainerCreating",
                                "PodInitializing"]
-IGNORABLE_STATUSES_REASONS = ["Evicted", ]
+IGNORABLE_STATUSES_REASONS = ["Evicted", "OutOfpods"]
 
 
 def search_for_pod_info(details, operator_id):
