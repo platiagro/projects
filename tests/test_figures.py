383be640a688f71c85920a1a49e5874555efb278
Fabio Beranizo Fontes Lopes <fabio.beranizo@gmail.com>
2021-10-28 13:28:51 -0300

    Refactors test_figures.py
    Mocks platiagro.list_figures and kfp.Client

--- tests/test_figures.py
+++ tests/test_figures.py
@@ -16,143 +16,80 @@ TEST_CLIENT = TestClient(app)
 class TestFigures(unittest.TestCase):
     maxDiff = None
 
-    # def setUp(self):
-    #     self.maxDiff = None
-    #     conn = engine.connect()
-    #     text = (
-    #         f"INSERT INTO projects (uuid, name, created_at, updated_at, tenant) "
-    #         f"VALUES (%s, %s, %s, %s, %s)"
-    #     )
-    #     conn.execute(
-    #         text,
-    #         (
-    #             PROJECT_ID,
-    #             NAME,
-    #             CREATED_AT,
-    #             UPDATED_AT,
-    #             TENANT,
-    #         ),
-    #     )
+    def setUp(self):
+        """
+        Sets up the test before running it.
+        """
+        util.create_mocks()
 
-    #     text = (
-    #         f"INSERT INTO experiments (uuid, name, project_id, position, is_active, created_at, updated_at) "
-    #         f"VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    #     )
-    #     conn.execute(
-    #         text,
-    #         (
-    #             EXPERIMENT_ID,
-    #             NAME,
-    #             PROJECT_ID,
-    #             POSITION,
-    #             1,
-    #             CREATED_AT,
-    #             UPDATED_AT,
-    #         ),
-    #     )
+    def tearDown(self):
+        """
+        Deconstructs the test after running it.
+        """
+        util.delete_mocks()
 
-    #     text = (
-    #         f"INSERT INTO tasks (uuid, name, description, image, commands, arguments, tags, parameters, "
-    #         f"experiment_notebook_path, deployment_notebook_path, cpu_limit, cpu_request, memory_limit, memory_request, "
-    #         f"readiness_probe_initial_delay_seconds, is_default, created_at, updated_at) "
-    #         f"VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    #     )
-    #     conn.execute(
-    #         text,
-    #         (
-    #             TASK_ID,
-    #             NAME,
-    #             DESCRIPTION,
-    #             IMAGE,
-    #             COMMANDS_JSON,
-    #             ARGUMENTS_JSON,
-    #             TAGS_JSON,
-    #             dumps([]),
-    #             EXPERIMENT_NOTEBOOK_PATH,
-    #             DEPLOYMENT_NOTEBOOK_PATH,
-    #             "100m",
-    #             "100m",
-    #             "1Gi",
-    #             "1Gi",
-    #             300,
-    #             0,
-    #             CREATED_AT,
-    #             UPDATED_AT,
-    #         ),
-    #     )
+    def test_list_figures_project_not_found(self):
+        """
+        Should return an http status 404 and an error message.
+        """
+        project_id = "unk"
+        experiment_id = "unk"
+        run_id = "unk"
+        operator_id = "unk"
 
-    #     text = (
-    #         f"INSERT INTO operators (uuid, name, status, status_message, experiment_id, task_id, parameters, created_at, updated_at) "
-    #         f"VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    #     )
-    #     conn.execute(
-    #         text,
-    #         (
-    #             OPERATOR_ID,
-    #             None,
-    #             "Unset",
-    #             None,
-    #             EXPERIMENT_ID,
-    #             TASK_ID,
-    #             PARAMETERS_JSON,
-    #             CREATED_AT,
-    #             UPDATED_AT,
-    #         ),
-    #     )
-    #     conn.close()
+        rv = TEST_CLIENT.get(
+            f"/projects/{project_id}/experiments/{experiment_id}/runs/{run_id}/operators/{operator_id}/figures"
+        )
+        result = rv.json()
+        expected = {"message": "The specified project does not exist"}
+        self.assertDictEqual(expected, result)
+        self.assertEqual(rv.status_code, 404)
 
-    #     t = np.arange(0.0, 2.0, 0.01)
-    #     s = 1 + np.sin(2 * np.pi * t)
-    #     fig, ax = plt.subplots()
-    #     ax.plot(t, s)
+    def test_list_figures_experiment_not_found(self):
+        """
+        Should return an http status 404 and an error message.
+        """
+        project_id = util.MOCK_UUID_1
+        experiment_id = "unk"
+        run_id = "unk"
+        operator_id = "unk"
 
-    #     platiagro.save_figure(
-    #         experiment_id=EXPERIMENT_ID,
-    #         operator_id=OPERATOR_ID,
-    #         run_id=RUN_ID,
-    #         figure=fig,
-    #     )
+        rv = TEST_CLIENT.get(
+            f"/projects/{project_id}/experiments/{experiment_id}/runs/{run_id}/operators/{operator_id}/figures"
+        )
+        result = rv.json()
+        expected = {"message": "The specified experiment does not exist"}
+        self.assertDictEqual(expected, result)
+        self.assertEqual(rv.status_code, 404)
 
-    # def tearDown(self):
-    #     prefix = "Experiments/{EXPERIMENT_ID}"
-    #     for obj in MINIO_CLIENT.list_objects(
-    #         BUCKET_NAME, prefix=prefix, recursive=True
-    #     ):
-    #         MINIO_CLIENT.remove_object(BUCKET_NAME, obj.object_name)
+    @mock.patch(
+        "kfp.Client",
+        return_value=util.MOCK_KFP_CLIENT,
+    )
+    @mock.patch(
+        "platiagro.list_figures",
+        return_value=[],
+    )
+    def test_list_figures_success(
+        self,
+        mock_list_figures,
+        mock_kfp_client,
+    ):
+        """
+        Should return a list of figures successfully.
+        """
+        project_id = util.MOCK_UUID_1
+        experiment_id = util.MOCK_UUID_1
+        run_id = "latest"
+        operator_id = util.MOCK_UUID_1
 
-    #     conn = engine.connect()
-    #     text = f"DELETE FROM operators WHERE experiment_id = '{EXPERIMENT_ID}'"
-    #     conn.execute(text)
+        rv = TEST_CLIENT.get(
+            f"/projects/{project_id}/experiments/{experiment_id}/runs/{run_id}/operators/{operator_id}/figures"
+        )
+        result = rv.json()
+        self.assertIsInstance(result, list)
 
-    #     text = f"DELETE FROM tasks WHERE uuid = '{TASK_ID}'"
-    #     conn.execute(text)
-
-    #     text = f"DELETE FROM experiments WHERE project_id = '{PROJECT_ID}'"
-    #     conn.execute(text)
-
-    #     text = f"DELETE FROM projects WHERE uuid = '{PROJECT_ID}'"
-    #     conn.execute(text)
-    #     conn.close()
-
-    # def test_list_figures(self):
-    #     rv = TEST_CLIENT.get(
-    #         f"/projects/1/experiments/unk/runs/unk/operators/{OPERATOR_ID}/figures"
-    #     )
-    #     result = rv.json()
-    #     expected = {"message": "The specified project does not exist"}
-    #     self.assertDictEqual(expected, result)
-    #     self.assertEqual(rv.status_code, 404)
-
-    #     rv = TEST_CLIENT.get(
-    #         f"/projects/{PROJECT_ID}/experiments/unk/runs/unk/operators/{OPERATOR_ID}/figures"
-    #     )
-    #     result = rv.json()
-    #     expected = {"message": "The specified experiment does not exist"}
-    #     self.assertDictEqual(expected, result)
-    #     self.assertEqual(rv.status_code, 404)
-
-    #     rv = TEST_CLIENT.get(
-    #         f"/projects/{PROJECT_ID}/experiments/{EXPERIMENT_ID}/runs/{RUN_ID}/operators/{OPERATOR_ID}/figures"
-    #     )
-    #     result = rv.json()
-    #     self.assertIsInstance(result, list)
+        mock_kfp_client.assert_any_call(host="http://ml-pipeline.kubeflow:8888")
+        mock_list_figures.assert_any_call(
+            experiment_id=experiment_id, operator_id=operator_id, run_id=run_id
+        )
