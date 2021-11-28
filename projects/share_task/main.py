19e5ef6855982a4787005b44c2347d16c45b72e8
Domingos <dfvneto@gmail.com>
2021-11-28 00:39:49 -0300

    Changed method used to send email

--- projects/share_task/main.py
+++ projects/share_task/main.py
@@ -8,11 +8,14 @@ import os
 import pkgutil
 import shutil
 import sys
-from typing import List
-import asyncio
+import smtplib
+import ssl
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email import encoders
 
 from jinja2 import Template
-from fastapi_mail import ConnectionConfig, FastMail, MessageSchema
 
 
 def run(source: str, emails: str, task_name: str, requested_at):
@@ -25,48 +28,51 @@ def run(source: str, emails: str, task_name: str, requested_at):
     emails : str
     """
     emails = emails.split(" ")
-    logging.info("Sharing Task.")
+    logging.info(f"Sharing Task {task_name}.")
     logging.info(f"source = {source}")
     logging.info(f"emails = {emails}")
     logging.info(f"requested_at = {requested_at}")
     logging.info(f"task_name = {task_name}")
-
     filename = "task"  # get source folder name (which should be the task name)
+
     # build the zipfile
     shutil.make_archive(filename, "zip", source)
 
-    email_message_template = pkgutil.get_data("projects", "config/email-template.html")
-
-    mail_username = os.getenv("MAIL_USERNAME", "")
-    mail_password = os.getenv("MAIL_PASSWORD", "")
     mail_sender_address = os.getenv("MAIL_SENDER_ADDRESS", "")
+    mail_password = os.getenv("MAIL_PASSWORD", "")
     mail_server = os.getenv("MAIL_SERVER", "")
-    mail_port = int(os.getenv("MAIL_PORT", 587))
-    mail_tls = bool(os.getenv("MAIL_TLS", True))
-    mail_ssl = bool(os.getenv("MAIL_SSL", False))
-    CONNECTION_CONFIG = ConnectionConfig(
-        MAIL_USERNAME=mail_username,
-        MAIL_PASSWORD=mail_password,
-        MAIL_FROM=mail_sender_address,
-        MAIL_PORT=mail_port,
-        MAIL_SERVER=mail_server,
-        MAIL_TLS=mail_tls,
-        MAIL_SSL=mail_ssl,
-        USE_CREDENTIALS=True,
-    )
+    mail_port = int(os.getenv("MAIL_PORT", 465))
 
-    body = make_email_message(email_message_template, task_name)
-    message = MessageSchema(
-        subject="Arquivos da tarefa",
-        recipients=emails,  # List of recipients, as many as you can pass
-        body=body,
-        attachments=[f"{filename}.zip"],
-        subtype="html",
-    )
-    fm = FastMail(CONNECTION_CONFIG)
-    loop = asyncio.get_event_loop()
-    loop.create_task(fm.send_message(message))
+    email_message_template = pkgutil.get_data("projects", "config/email-template.html")
+    html_string = make_email_message(email_message_template, task_name)
+    text = MIMEText(html_string, "html")
 
+    message = MIMEMultipart()
+
+    with open(f"{filename}.zip", "rb") as attachment:
+        # Add file as application/octet-stream
+        # Email client can usually download this automatically as attachment
+        part = MIMEBase("application", "octet-stream")
+        part.set_payload(attachment.read())
+
+    # Encode file in ASCII characters to send by email
+    encoders.encode_base64(part)
+
+    # Add header as key/value pair to attachment part
+    part.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}.zip",
+    )
+    message.attach(part)
+    message.attach(text)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(mail_server, mail_port, context=context) as server:
+        server.login(mail_sender_address, mail_password)
+        for email in emails:
+            server.sendmail(
+                mail_sender_address, email, message.as_string()
+            )
     logging.info("Done!")
 
 
@@ -105,10 +111,10 @@ def parse_args(args):
         "--emails", type=str, help="List of emails",
     )
     parser.add_argument(
-        "--task-name", type=str, help="Task's name",
+        "--task-name", type=str, default="task", help="Task's name",
     )
     parser.add_argument(
-        "--requested-at", type=str, help="Request time",
+        "--requested-at", type=str, default="", help="Request time",
     )
     parser.add_argument(
         "--log-level",
