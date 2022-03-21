32db16fe6a7859a395ad5b56a729af16582a78ea
Domingos <dfvneto@gmail.com>
2022-03-21 15:38:10 -0300

    changed the way the files are sent to the pod

--- projects/kubernetes/notebook.py
+++ projects/kubernetes/notebook.py
@@ -448,42 +448,29 @@ def handle_task_creation(
         destination_path = f"{JUPYTER_WORKSPACE}/{task.name}/"
         copy_files_in_pod(source_path, destination_path)
     else:
-        if task.experiment_notebook and task.deployment_notebook:
-            experiment_path = f"{task.name}/Experiment.ipynb"
-            with NamedTemporaryFile("w", delete=False) as f1:
-                json.dump(task.experiment_notebook, f1)
+        filepaths = list()
 
-            filepath1 = f1.name
-
-            deployment_path = f"{task.name}/Deployment.ipynb"
-            with NamedTemporaryFile("w", delete=False) as f2:
-                json.dump(task.deployment_notebook, f2)
-
-            filepath2 = f2.name
-
-            copy_files_to_pod([filepath1, filepath2], [experiment_path, deployment_path])
-            os.remove(filepath1)
-            os.remove(filepath2)
-        
-        elif task.experiment_notebook and not task.deployment_notebook:
+        if task.experiment_notebook:
             experiment_path = f"{task.name}/Experiment.ipynb"
             # copies experiment notebook file to pod
             with NamedTemporaryFile("w", delete=False) as f:
                 json.dump(task.experiment_notebook, f)
 
             filepath = f.name
-            copy_file_to_pod(filepath, experiment_path)
-            os.remove(filepath)
+            filepaths.append((filepath, experiment_path))
 
-        elif task.deployment_notebook and not task.experiment_notebook:
+        if task.deployment_notebook:
             deployment_path = f"{task.name}/Deployment.ipynb"
             # copies deployment notebook file to pod
             with NamedTemporaryFile("w", delete=False) as f:
                 json.dump(task.deployment_notebook, f)
 
             filepath = f.name
-            copy_file_to_pod(filepath, deployment_path)
-            os.remove(filepath)
+            filepaths.append((filepath, deployment_path))
+
+        copy_files_to_pod(filepaths)
+        for path in filepaths:
+            os.remove(path[0])
 
     # create ConfigMap for monitoring tasks
     if MONITORING_TAG in task.tags:
@@ -700,7 +687,7 @@ def copy_file_to_pod(filepath, destination_path):
     api_instance = client.CoreV1Api()
 
     # The following command extracts the contents of STDIN to /home/jovyan/tasks
-    exec_command = ["tar", "xvf", "-", "-C", "/home/jovyan/tasks"]
+    exec_command = ["tar", "xvf", "-", "-C", JUPYTER_WORKSPACE]
     while True:
         try:
             pod = api_instance.read_namespaced_pod(
@@ -749,7 +736,7 @@ def copy_file_to_pod(filepath, destination_path):
     warnings.warn(f"Copied '{filepath}' to '{destination_path}'!")
 
 
-def copy_files_to_pod(filepaths, destination_path):
+def copy_files_to_pod(filepaths):
     """
     Copies a local file to a pod in notebook server.
     Based on this example:
@@ -757,15 +744,13 @@ def copy_files_to_pod(filepaths, destination_path):
 
     Parameters
     ----------
-    filepath : str
-    destination_path : str
+    filepath : list
     """
-    warnings.warn(f"Copying '{filepaths[0]} and {filepaths[1]}' to '{destination_path}'...")
     load_kube_config()
     api_instance = client.CoreV1Api()
 
     # The following command extracts the contents of STDIN to /home/jovyan/tasks
-    exec_command = ["tar", "xvf", "-", "-C", "/home/jovyan/tasks"]
+    exec_command = ["tar", "xvf", "-", "-C", JUPYTER_WORKSPACE]
     while True:
         try:
             pod = api_instance.read_namespaced_pod(
@@ -804,15 +789,13 @@ def copy_files_to_pod(filepaths, destination_path):
     with TemporaryFile() as tar_buffer:
         # Prepares an uncompressed tarfile that will be written to STDIN
         with tarfile.open(fileobj=tar_buffer, mode="w") as tar:
-            tar.add(filepaths[0], arcname=destination_path[0])
-            tar.add(filepaths[1], arcname=destination_path[1])
+            for path in filepaths:
+                tar.add(path[0], arcname=path[1])
 
         # Rewinds to beggining of tarfile
         tar_buffer.seek(0)
 
         handle_container_stream(container_stream=container_stream, buffer=tar_buffer)
-
-    warnings.warn(f"Copied '{filepaths[0]} and {filepaths[1]}' to '{destination_path[0]} and {destination_path[1]}'!")
 
 
 def copy_files_in_pod(source_path, destination_path):
