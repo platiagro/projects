1779a5d7ad5a407b1d0769c6a13e315ef26801c3
Andre Luiz Ferreira da Silva Nogueira <aluizfs@avelar.aquarius.cpqd.com.br>
2022-03-23 14:16:07 -0300

    More tests

--- projects/schemas/project.py
+++ projects/schemas/project.py
@@ -5,52 +5,15 @@ from datetime import datetime
 from typing import List, Optional
 
 from pydantic import BaseModel, validator
-from projects.exceptions import BadRequest
 
 from projects.schemas.deployment import Deployment
 from projects.schemas.experiment import Experiment
+from projects.schemas import validators
 from projects.utils import to_camel_case
 
 FORBIDDEN_CHARACTERS_REGEX = "[!*'():;@&=+$,\/?%#\[\]]"
 MAX_CHARS_ALLOWED = 50
-
-def raise_if_exceeded(max_chars_allowed, value):
-    if len(value) > max_chars_allowed:
-        raise BadRequest(
-            code="ExceededACharAmount",
-            message="Char quantity exceeded maximum allowed",
-            )
-   
-def raise_if_forbidden_character(forbidden_char_regex, value):
-        if re.findall(forbidden_char_regex, value):
-            raise BadRequest(
-                code="NotAllowedChar",
-                message="Not allowed char in search field",
-            )
-
-# This is necessary to mysql consider special character
-# maybe this logic won't work on another database
-# maybe we can refactor this!            
-def escaped_format(value):
-        """
-        This function will escape string so mysql database be able to read special characters.
-
-        Parameters
-        ----------
-        value: str
-        Returns
-        -------
-        str
-            string escaped
-        """
-        escaped_string = ""
-        # to avoid the trouble of identify every special character we gonna escape all!
-        for character in value:
-            if character.isalnum():
-                escaped_string = escaped_string + character
-            else:
-                escaped_string = escaped_string + "\\" + character
-        return escaped_string            
+MAX_CHARS_ALLOWED_DESCRIPTION = 300
 
 class ProjectBase(BaseModel):
     class Config:
@@ -65,8 +28,13 @@ class ProjectCreate(ProjectBase):
     
     @validator("name")
     def validate_name(cls, v):
-        raise_if_exceeded(MAX_CHARS_ALLOWED,v)
-        raise_if_forbidden_character(FORBIDDEN_CHARACTERS_REGEX,v)
+        validators.raise_if_exceeded(MAX_CHARS_ALLOWED,v)
+        validators.raise_if_forbidden_character(FORBIDDEN_CHARACTERS_REGEX,v)
+        return v
+   
+    @validator("description")
+    def validate_description(cls, v):
+        validators.raise_if_exceeded(MAX_CHARS_ALLOWED_DESCRIPTION, v)
         return v
         
 #Request
@@ -124,7 +92,7 @@ class ProjectListRequest(BaseModel):
     def validate_name_in_filters(cls, v):
         if v.get("name"):
             name = v.get("name")
-            raise_if_exceeded(MAX_CHARS_ALLOWED, name)
-            raise_if_forbidden_character(FORBIDDEN_CHARACTERS_REGEX, name)
-            v['name'] = escaped_format(name)
+            validators.raise_if_exceeded(MAX_CHARS_ALLOWED, name)
+            validators.raise_if_forbidden_character(FORBIDDEN_CHARACTERS_REGEX, name)
+            v['name'] = validators.escaped_format(name)
         return v
